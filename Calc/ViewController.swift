99f9bdbd5fd5912a1eca8a3db386a3bd985d9bfb
Elijah Silang <iv.elijah.s@gmail.com>
2021-02-01 11:58:50 -0600

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Elijah Silang on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
